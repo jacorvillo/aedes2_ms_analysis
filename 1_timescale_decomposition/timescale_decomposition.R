3cdbb7aef194464d0534d55dd26243a8d2d46481
Javier Corvillo <jacorvillo@protonmail.com>
2025-03-25 10:14:34 +0100

    Refactor timescale decomposition script to use GAM for trend extraction, implement Butterworth filter for decadal component, and update output file names

--- 1_timescale_decomposition/timescale_decomposition.R
+++ 1_timescale_decomposition/timescale_decomposition.R
@@ -1,13 +1,13 @@
 # timescale_decomposition.R
 
 #' @description This script reads the R0 monthly data from the AeDES2's Monitoring System and 
-#' decomposes each individual grid point's time series into trend, seasonal, and decadal 
+#' decomposes each individual grid point's time series into trend, seasonal, and remainder 
 #' components, calculating the variance explained by each component and plotting the results on 
 #' maps. The SSSRs are later obtained by checking which regions in Iturbide et al., 2020 
 #' correspond to the regions with the highest seasonal variance obtained in this script.
 
 # Set the working directory
-setwd("C:/Users/jcorvill/Documents/Obsidian/vscode/github/monitoring_system_analysis/")
+setwd("C:/Users/jcorvill/Documents/vscode/github/monitoring_system_analysis/")
 
 # Necessary packages for the script:
 library(s2dv)
@@ -15,6 +15,8 @@ library(viridis)
 library(dplyr)
 library(zoo)
 library(ncdf4)
+library(mgcv)
+library(signal)
 
 # Source R functions:
 source("0_functions/r_functions.R")
@@ -64,59 +66,75 @@ percentage_trend <- array(NA, dim = c(length(lat), length(lon)))
 percentage_seasonal <- array(NA, dim = c(length(lat), length(lon)))
 percentage_decadal <- array(NA, dim = c(length(lat), length(lon)))
 
+detrended_save <- array(NA, dim = c(504, length(lat), length(lon)))
+
+# Load global temperature signal from ncdf file
+nc_file <- nc_open("0_data/tas_median/detrended_monthly_data.nc")
+detrended_temps <- ncvar_get(nc_file, "detrended_temps") + 273.15 # Convert to Celsius
 
 # Loop through each grid point and decompose the time series into trend, seasonal, 
-# and decadal components
+# and decadal components (interannual variability, subdecadal variability, etc.)
+
+# Sampling rate (e.g., 1 for annual data, 12 for monthly data)
+sampling_rate <- 12  # Annual data
+
+# Cutoff frequency for a period of 10 years
+cutoff_frequency <- 1 / 10  # 1/10 cycles per year for a 10-year period
 
 for (nlat in seq_along(lat)) {
   for (nlon in seq_along(lon)) {
 
-    # Extract the time series for the current grid point
-    signal <- ts(combined_data[, nlat, nlon], frequency = 12, start = c(1980, 1))
+    if (all(is.na(combined_data[, nlat, nlon]))) { # If all values are NA, set the values to NA
+      var_original[nlat, nlon] <- NA
+      var_trend[nlat, nlon] <- NA
+      var_seasonal[nlat, nlon] <- NA
+      var_decadal[nlat, nlon] <- NA
 
-    if (any(is.na(combined_data[, nlat, nlon]))) {
-      if (all(is.na(combined_data[, nlat, nlon]))) { # If all values are NA, set the values to NA
-        var_original[nlat, nlon] <- NA
-        var_trend[nlat, nlon] <- NA
-        var_seasonal[nlat, nlon] <- NA
-        var_decadal[nlat, nlon] <- NA
+      total_test[nlat, nlon] <- NA
+      percentage_trend[nlat, nlon] <- NA
+      percentage_seasonal[nlat, nlon] <- NA
+      percentage_decadal[nlat, nlon] <- NA
 
-        total_test[nlat, nlon] <- NA
-        percentage_trend[nlat, nlon] <- NA
-        percentage_seasonal[nlat, nlon] <- NA
-        percentage_decadal[nlat, nlon] <- NA
+    } else { 
 
-      } else { # If there are some NA values, set those to 0
-        
-        signal[is.na(signal)] <- 0
+      # Extract the time series for the current grid point
+      signal <- combined_data[, nlat, nlon]
+      signal[is.na(signal)] <- 0 # If there are some NA values, set those to 0
 
-        # Decompose the time series into trend, seasonal, and decadal components
-        
-        components <- stl(signal, s.window = "per", t.window = 120)
+      # Trend component: Regress the R0 data to the temperature signal obtained in temp_trend.R
+      model <- gam(signal ~ s(detrended_temps, k = 246))
 
-        var_original[nlat, nlon] <- var(combined_data[, nlat, nlon], na.rm = TRUE)
-        var_trend[nlat, nlon] <- var(components$time.series[, 2], na.rm = TRUE)
-        var_seasonal[nlat, nlon] <- var(components$time.series[, 1], na.rm = TRUE)
-        var_decadal[nlat, nlon] <- var(components$time.series[, 3], na.rm = TRUE)
+      # Extract the trend component, convert the trend from R0/temperature to R0/month
+      trend_signal <- predict(model) * detrended_temps / 504
 
-        total_test[nlat, nlon] <- var_original[nlat, nlon] - 
-          (var_trend[nlat, nlon] + var_seasonal[nlat, nlon] + var_decadal[nlat, nlon])
+      # Decadal component: Apply a low-pass Butterworth filter to the signal w/o the trend
+      detrended_data <- signal - trend_signal
 
-        percentage_trend[nlat, nlon] <- var_trend[nlat, nlon] / var_original[nlat, nlon] * 100
-        percentage_seasonal[nlat, nlon] <- var_seasonal[nlat, nlon] / var_original[nlat, nlon] * 100
-        percentage_decadal[nlat, nlon] <- var_decadal[nlat, nlon] / var_original[nlat, nlon] * 100
-      
-      }
-    } else {
-      
-      # Decompose the time series into trend, seasonal, and decadal components
-      
-      components <- stl(signal, s.window = "per", t.window = 120)
+      # Detrended data: Remove the trend from the original signal
+      detrended_save[, nlat, nlon] <- detrended_data
+
+      # Design the Butterworth filter
+      filter_coefficients <- butter(2, cutoff_frequency / (sampling_rate / 2), type = "low")
+
+      # Mirror padding approach
+      pad_length <- 100  # Choose appropriate padding length
+      padded_signal <- c(rev(detrended_data[1:pad_length]), detrended_data, 
+        rev(detrended_data[(length(detrended_data)-pad_length+1):length(detrended_data)])
+      )
+
+      # Filter padded signal
+      padded_result <- filter(filter_coefficients, padded_signal)
+
+      # Extract the valid portion
+      decadal_signal <- padded_result[(pad_length+1):(length(padded_result)-pad_length)]
+
+      # Extract the seasonal component
+      seasonal_signal <- detrended_data - decadal_signal
 
       var_original[nlat, nlon] <- var(combined_data[, nlat, nlon], na.rm = TRUE)
-      var_trend[nlat, nlon] <- var(components$time.series[, 2], na.rm = TRUE)
-      var_seasonal[nlat, nlon] <- var(components$time.series[, 1], na.rm = TRUE)
-      var_decadal[nlat, nlon] <- var(components$time.series[, 3], na.rm = TRUE)
+      var_trend[nlat, nlon] <- var(trend_signal, na.rm = TRUE)
+      var_seasonal[nlat, nlon] <- var(seasonal_signal, na.rm = TRUE)
+      var_decadal[nlat, nlon] <- var(decadal_signal, na.rm = TRUE)
 
         total_test[nlat, nlon] <- var_original[nlat, nlon] - 
           (var_trend[nlat, nlon] + var_seasonal[nlat, nlon] + var_decadal[nlat, nlon])
@@ -132,6 +150,7 @@ for (nlat in seq_along(lat)) {
       percentage_decadal[nlat, nlon] <- NA
     
     }
+    cat("Processing grid point (", nlat, ",", nlon, ")\n") # Print the progress
   }
 }
 
@@ -141,12 +160,12 @@ rainbow <- colorRampPalette(viridis(10, option = "turbo"))
 
 toptitle_trend <- "Variance % Explained by Trend"
 toptitle_seasonal <- "Variance % Explained by Seasonal Variability"
-toptitle_decadal <- "Variance % Explained by Decadal Variability"
+toptitle_decadal <- "Variance % Explained by Remaining Variability"
 
-fileout_trend <- paste0("data/trend_global3.png")
-fileout_seasonal <- paste0("data/seasonal_global3.png")
-fileout_decadal <- paste0("data/decadal_global3.png")
-fileout_merged <- paste0("data/merged_global3.png")
+fileout_trend <- paste0("data/trend_global.png")
+fileout_seasonal <- paste0("data/seasonal_global.png")
+fileout_decadal <- paste0("data/remainder_global.png")
+fileout_merged <- paste0("data/merged_global.png")
 
 # Plot the maps
 
@@ -165,7 +184,7 @@ s2dv::PlotEquiMap(
   bar_tick_scale = 0.5,
   axes_tick_scale = 0.5,
   axes_label_scale = 0.5,
-  fileout = fileout_trend,
+  # fileout = fileout_trend,
   width = 8,
   height = 8,
   size_units = "in"
@@ -186,7 +205,7 @@ s2dv::PlotEquiMap(
   bar_tick_scale = 0.5,
   axes_tick_scale = 0.5,
   axes_label_scale = 0.5,
-  fileout = fileout_seasonal,
+  # fileout = fileout_seasonal,
   width = 8,
   height = 8,
   size_units = "in",
@@ -207,31 +226,13 @@ s2dv::PlotEquiMap(
   bar_tick_scale = 0.5,
   axes_tick_scale = 0.5,
   axes_label_scale = 0.5,
-  fileout = fileout_decadal,
+  # fileout = fileout_decadal,
   width = 8,
   height = 8,
   size_units = "in"
 )
 
-detrended_data <- array(NA, dim = c(504, length(lat), length(lon)))
-
-# Create a time vector for the x-axis
-time_vector <- seq(1, dim(combined_data)[1], by = 1)
-
-for (nlat in seq_along(lat)) {
-  for (nlon in seq_along(lon)) {
-    if (all(is.na(combined_data[, nlat, nlon]))) { # If all values are NA, set the values to NA
-      detrended_data[, nlat, nlon] <- NA
-    } else { # If there are some NA values, set those to 0
-      combined_data[is.na(combined_data[, nlat, nlon]), nlat, nlon] <- 0
-      # Detrend the data using LOESS smoothing with a span of 120 months
-      # and store the detrended data in the new array
-      detrended_data[, nlat, nlon] <- combined_data[, nlat, nlon] - predict(loess(combined_data[, nlat, nlon] ~ time_vector, span = 120 / 504))
-    }
-  }
-}
-
-norm <- normalize(detrended_data[3:503, , ]) # Trimming to account for incomplete seasons
+detrended_save <- detrended_save[3:503, , ] # Trimming to account for incomplete seasons
 
 # --- Save the combined data to a NetCDF file --- #
 
@@ -242,7 +243,7 @@ dim_lon <- ncdim_def("lon", "degrees_east", lon)
 
 # Define the variable
 var_norm_data <- ncvar_def("detrended_temps", "units", list(dim_time, dim_lat, dim_lon),
-  -9999, longname = "Detrended R0 Values (Normalized)", prec = "double"  
+  -9999, longname = "Detrended R0 Values", prec = "double"  
 )
 
 # Create the NetCDF file
@@ -251,7 +252,7 @@ nc_file <- nc_create(
 var_norm_data)
 
 # Write the data to the NetCDF file
-ncvar_put(nc_file, var_norm_data, norm)
+ncvar_put(nc_file, var_norm_data, detrended_save)
 
 # Close the NetCDF file
 nc_close(nc_file)
